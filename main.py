88bd01dc882805858f7c572f4e5066525afe3077
ShivaBasava <shivapd1996@gmail.com>
2022-01-04 20:35:22 +0530

    Grid Layout

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
+
+
+# class GridLayoutExample(GridLayout): # inside .kv file, directly accessing it by @
+#     pass
 
 
 class AnchorLayoutExample(AnchorLayout):
